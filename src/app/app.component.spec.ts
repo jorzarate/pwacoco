641b0b13872203cb35bcf2a723e43c93b315fe21
Jorge Zárate <jorgezarate@gmail.com>
2020-05-27 12:52:09 -0300

    cambio a digitalCode v0.0.1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'digitalCode'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pwacoco');
+    expect(app.title).toEqual('digitalCode');
   });
 
   it('should render title', () => {
